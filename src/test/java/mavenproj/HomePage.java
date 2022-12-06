ef98252cfc18cdd5f108c3894a7221ae9b4aa2c4
kiran <kirankatkar3317@gmail.con>
2022-12-06 23:33:48 +0530

    changes in devop in testing

--- src/test/java/mavenproj/HomePage.java
+++ src/test/java/mavenproj/HomePage.java
@@ -33,6 +33,7 @@ public class HomePage extends base {
 		System.out.println("kk");
 		System.out.println("kk1");
 		System.out.println("kk2");
+		System.out.println("kk3");
 		lp.Button().click();
 	}
 
